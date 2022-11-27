9a258e7e8bba59382dab00f45604bc8f252d9420
RAKKII-san <schuang2001@gmail.com>
2022-11-27 00:23:05 -0800

    Day 2: ALU Control + Instruction Memory done

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ This project consists of a single-cycle implementation of a MIPS processor
 in Verilog. It handles a basic subset of instructions: ALU functions, lw/sw,
 beq, jump.
 
+A txt file is included to test out the Verilog processor. This was also provided by
+fpga4student.com. Screenshots from local testing are provided by group members. 
+
 Resources used: 
 * https://www.fpga4student.com/2017/06/32-bit-pipelined-mips-processor-in-verilog-1.html
--- alu_control.v
+++ alu_control.v
@@ -1,22 +1,20 @@
 // Submodule: ALU Control Unit
 
-module ALUCtrlrol(
-    input [1:0] ALUOp,
-    input [5:0] funct,
-    output reg [2:0] ALUCtrl
-);
-always @(*)
-    case(ALUOp)
-        2'b00: ALUCtrl <= 3'b010; // add for lw/sw
-        2'b01: ALUCtrl <= 3'b110; // sub for beq
-        default: case (funct) // R-type instructions
-            6'b100000: ALUCtrl <= 3'b010; // add
-            6'b100010: ALUCtrl <= 3'b010; // sub
-            6'b100100: ALUCtrl <= 3'b010; // logical and
-            6'b100101: ALUCtrl <= 3'b010; // logical or
-            6'b101010: ALUCtrl <= 3'b010; // set on less (slt)
-            default: ALUCtrl <= 3'b101; // should never happen
-        endcase
+module ALUControl(ALUControl, ALUOp, Funct);
+    output [1:0] ALUControl;
+    reg [1:0] ALUControl;
+    input [1:0] ALUOp;
+    input [5:0] Funct;
+    wire [7:0] ALUControlIn;
+    assign ALUControlIn = {ALUOp,Funct};
+    always @(ALUControlIn)
+    casex (ALUControlIn)
+        8'b11xxxxxx: ALUControl=2'b01; // xor for xori
+        8'b00xxxxxx: ALUControl=2'b00; // add for lw or sw
+        8'b01xxxxxx: ALUControl=2'b10; // sub for bne
+        8'b10100000: ALUControl=2'b00; // add
+        8'b10100010: ALUControl=2'b10; // sub
+        8'b10101010: ALUControl=2'b11; // slt
+        default: ALUControl=2'b00; // shouldn't happen
     endcase
-
 endmodule
