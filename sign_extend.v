193f44537d63e55730d1e3d48b42442f08105543
C-AJ <89857745+C-AJ@users.noreply.github.com>
2022-12-04 15:19:00 -0800

    update

--- sign_extend.v
+++ sign_extend.v
@@ -6,10 +6,3 @@ output [31:0] sOut32;
 input [15:0] sIn16;
 assign sOut32 = {{16{sIn16[15]}},sIn16};
 endmodule
-// Shift left 2 module 
-module shift_left_2(Out32, In32);
-output [31:0] Out32;
-input [31:0] In32;
-
-assign Out32 = {In32[29:0],2'b00};
-endmodule
